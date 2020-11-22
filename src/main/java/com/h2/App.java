c51d16524e8a0c4e023e0f55cba866512b77b73f
adnank9 <43317054+adnank9@users.noreply.github.com>
2020-11-22 22:07:36 +0530

    Update App.java

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -17,7 +17,7 @@ public class App
     
     private static int add(int[] numbers){
         int sum =0;
-        for(int i=0;i<=numbers;i--){
+        for(int i=0;i<=numbers;i++){
         sum +=numbers[i];
         }
         return sum;
